b7d144ca14a00605eb14eeddc80447314aee185b
kangtae49 <kangtae49@gmail.com>
2021-01-06 20:52:12 +0900

    class

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: 'kkt',
-  gender: 'male',
-  age: 22,
-};
+const lynn = new Human('kkt', 18, 'femain');
 
 const sayHi = (person: Human): string => {
   return `${person.name} ${person.age} ${person.gender}!`;
 };
 
 // console.log(sayHi('kkt', 444, 'male'));
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
