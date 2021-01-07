41c965188d72deb1eebbba8ef005ec6d9c7191a0
kangtae49 <kangtae49@gmail.com>
2021-01-07 15:49:36 +0900

    last

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `${name} ${age} ${gender}`;
+};
+console.log(sayHi('kkt', 444, 'male'));
+//# sourceMappingURL=index.js.map
